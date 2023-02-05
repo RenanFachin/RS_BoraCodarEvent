de64bb1e1e4d91e9fd6ee3b84f230406c145d431
Renan <renan.fachin@hotmail.com>
2023-02-05 08:35:40 -0300

    adds effects on click

--- desafio05_calculadora/src/components/Button.tsx
+++ desafio05_calculadora/src/components/Button.tsx
@@ -13,7 +13,7 @@ export function Button({ digit, enterDigit, buttonColor = 'default', className, 
     
     return (
         <button className={clsx(
-            `w-12 h-12 md:w-16 md:h-16 rounded-full text-2xl flex items-center justify-center text-lightMode-text dark:text-gray-100 shadow-calculatorButtonShadow bg-calculatorButtons`, className)}
+            `w-12 h-12 md:w-16 md:h-16 rounded-full text-2xl flex items-center justify-center text-lightMode-text dark:text-gray-100 shadow-calculatorButtonShadow bg-calculatorButtons hover:brightness-200 active:translate-y-[2px]`, className)}
              onClick={() => enterDigit(digit)}
             {...rest}
         >
